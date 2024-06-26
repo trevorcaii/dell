99c16e773ecd8f5444b7105fbfd5ed236bbaa4b6
trevorcaii <111313569+trevorcaii@users.noreply.github.com>
2024-06-26 11:43:35 -0500

    Update train_time.py

--- train_time.py
+++ train_time.py
@@ -15,9 +15,23 @@ def read_link_status(bus):
     except subprocess.CalledProcessError:
         return None
     
+def read_link_capabilities18(bus):
+    try:
+        link_capabilities_output = subprocess.check_output(["setpci", "-s", bus, "CAP_EXP+0X0c.l"])
+        return link_capabilities_output.decode().strip()
+    except subprocess.CalledProcessError:
+        print("error")
+        return None
+    
 def hex_to_binary(hex_string):
     binary_string = format(int(hex_string, 16), '032b')
     return binary_string
+
+def extract_link_capabilities(hex_string):
+    binary_string = hex_to_binary(hex_string)
+    max_link_width = int(binary_string[-3:], 2)
+    max_link_speed = int(binary_string[-9:-4], 2)
+    return max_link_width, max_link_speed
 
 def extract_link_status(hex_string):
     binary_string = hex_to_binary(hex_string)
@@ -39,12 +53,22 @@ def get_train_time(bdf):
         print("Invalid BDF")
         return -1
     
+    link_status_bits = []
+
     set_bridge_control(bdf, "0043", "Dell1234")
-    link_status_bits = hex_to_binary(read_link_status(bdf))
+    link_status_bits.append(hex_to_binary(read_link_status(bdf)))
     print(link_status_bits)
     set_bridge_control(bdf, "0003", "Dell1234")
-    link_status_bits = hex_to_binary(read_link_status(bdf))
-    print(link_status_bits)
+    link_status_bits.append(hex_to_binary(read_link_status(bdf)))
+    link_status_bits.append(hex_to_binary(read_link_status(bdf)))
+    link_status_bits.append(hex_to_binary(read_link_status(bdf)))
+    time.sleep(2)
+    link_status_bits.append(hex_to_binary(read_link_status(bdf)))
+
+    for link in link_status_bits:
+        print(link)
+
+    
 
 
 
