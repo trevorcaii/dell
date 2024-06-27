b1bba955ae174d517313b5db438cd8833524ef96
trevorcaii <111313569+trevorcaii@users.noreply.github.com>
2024-06-27 11:43:20 -0500

    Update train_time.py

--- train_time.py
+++ train_time.py
@@ -1,181 +1,93 @@
 import subprocess
-from datetime import datetime
 import time
 
-def execute_shell_command(command):
+def read_header(bus):
     try:
-        # Execute the shell command
-        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if result.returncode == 0:
-            # Successful execution
-            return result.stdout.decode("utf-8").strip()  # Decode bytes to string
-        else:
-            # Error occurredhttps://github.com/manunicholasjacob/Testing/blob/main/getbdf2
-            return f"Error: {result.stderr.decode('utf-8').strip()}"  # Decode bytes to string
-    except Exception as e:
-        return f"Error: {str(e)}"
-
-
-def check_replay(gpu_percentage = 95, burn_time = 3600, gpu_number = 0, gpu_index = [], call_time = 10):
-    try:
-        print("Starting gpu_burn...")
-        # "> /dev/null" will not clutter stdout with gpu_burn's outputs
-        gpu_process = subprocess.Popen(['/home/NVIDIA/gpu_burn-1.1/gpu-burn/gpu_burn', '-d', '-m', f"{gpu_percentage}%", f"{burn_time}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("running in background")
-    except Exception as e:
-        return f"Error: {str(e)}"
-
-    # Periodically execute another command while './gpu_burn' is running
-    replay_count = ""
-    while gpu_process.poll() is None:
-        now = datetime.now()
-        print("Current Timestamp:", now)
-        if(len(gpu_index) > 0):
-            for index in gpu_index:
-                print(f"GPU {index}:")
-                replay_count = execute_shell_command(f"nvidia-smi -i {index} -q|grep -i replay")
-                replay_count = replay_count.split("\n")
-                for line in replay_count: print(line.strip())
-        else:
-            for i in range(gpu_number):
-                print(f"GPU {i}:")
-                replay_count = execute_shell_command(f"nvidia-smi -i {i} -q|grep -i replay")
-                replay_count = replay_count.split("\n")
-                for line in replay_count: print(line.strip())
-        print()
-        time.sleep(call_time) 
-
-    # 'gpu_burn' has finished; you can perform any cleanup or final actions here
-    print("gpu_burn has completed.")
-    bdf_read = execute_shell_command("nvidia-smi --query-gpu=pci.bus_id --format=csv,noheader")
-    bdf_read = bdf_read.split('\n')
-    bdf_read = [":".join(line.split(':')[1:]) for line in bdf_read]
-    with open("/home/dell/Desktop/output.txt","w") as file:
-        if(len(gpu_index) > 0):
-            bdfs = []
-            for i, bdf in enumerate(bdf_read):
-                if i in gpu_index: bdfs.append(bdf)
-            for i, bdf in enumerate(bdfs):
-                file.write(f"GPU {gpu_index[i]} - " + bdf + ":\n")
-                replay_count = execute_shell_command(f"nvidia-smi -i {gpu_index[i]} -q|grep -i replay")
-                replay_count = replay_count.split("\n")
-                for line in replay_count: file.write(line.strip() + "\n")
-                file.write("\n")
-        else:
-            for gpu_index_tag, bdf in enumerate(bdf_read): 
-                file.write(f"GPU {gpu_index_tag} - " + bdf + ":\n")
-                replay_count = execute_shell_command(f"nvidia-smi -i {gpu_index_tag} -q|grep -i replay")
-                replay_count = replay_count.split("\n")
-                for line in replay_count: file.write(line.strip() + "\n")
-                file.write("\n")
-    stdout, stderr = gpu_process.communicate()
-    with open("/home/dell/Desktop/gpu_burn_output", "w") as file:
-        file.write(stdout.decode("utf-8"))
-def run_command(command):
-    result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = result.communicate()
-    if result.returncode != 0:
-        raise Exception(f"Command failed with error: {stderr.decode('utf-8')}")
-    return stdout.decode('utf-8')
-
-def get_bdf_list():
-    """ Get a list of all BDFs using lspci. """
-    output = run_command("lspci")
-    bdf_list = [line.split()[0] for line in output.splitlines()]
-    return bdf_list
-
-def get_vendor_bdf_list(vendor_id):
-    """ Get a list of BDFs for a specific vendor using lspci. """
-    output = run_command(f"lspci -d {vendor_id}:")
-    vendor_bdf_list = [line.split()[0] for line in output.splitlines()]
-    return vendor_bdf_list
-
-def get_header_type(bdf):
-    
-    header_type = run_command(f"setpci -s {bdf} HEADER_TYPE")
-    return header_type.strip()
-
-def get_secondary_bus_number(bdf):
-    """ Get the secondary bus number for a given BDF using setpci. """
-    secondary_bus_number = run_command(f"setpci -s {bdf} SECONDARY_BUS")
-    return secondary_bus_number.strip()
-
-def read_slot_capabilities(bdf):
-    try:
-        slot_capabilities_output = subprocess.check_output(["setpci", "-s", bdf, "CAP_EXP+0X14.l"])
-        return slot_capabilities_output.decode().strip()
+        bridge_control_output = subprocess.check_output(["setpci", "-s", bus, "0e.w"])
+        return f" : {bridge_control_output.decode().strip()}"
     except subprocess.CalledProcessError:
+        return f"Error reading Bridge Control for {bus}."
+    
+def read_link_status(bus):
+    try:
+        link_status_output = subprocess.check_output(["setpci", "-s", bus, "CAP_EXP+0X12.w"])
+        return link_status_output.decode().strip()
+    except subprocess.CalledProcessError:
+        return None
+    
+def read_link_capabilities(bus):
+    try:
+        link_capabilities_output = subprocess.check_output(["setpci", "-s", bus, "CAP_EXP+0X0c.l"])
+        return link_capabilities_output.decode().strip()
+    except subprocess.CalledProcessError:
+        print("error")
         return None
     
 def hex_to_binary(hex_string):
     binary_string = format(int(hex_string, 16), '032b')
     return binary_string
 
-def gpu_traverse_up():
-    # use nvidia-smi to get all the BDF of the GPUs
-    bdf_read = execute_shell_command("nvidia-smi --query-gpu=pci.bus_id --format=csv,noheader")
-    bdf_read = bdf_read.split('\n')
-    bdf_read = [":".join(line.split(':')[1:]) for line in bdf_read]
-    gpu_bdf_list = [bdf.lower() for bdf in bdf_read]
+def extract_link_capabilities(hex_string):
+    binary_string = hex_to_binary(hex_string)
+    max_link_width = int(binary_string[-3:], 2)
+    max_link_speed = int(binary_string[-9:-4], 2)
+    return max_link_width, max_link_speed
 
-    # get a list of all bdfs
-    all_bdf_list = get_bdf_list()
+def extract_link_status(hex_string):
+    binary_string = hex_to_binary(hex_string)
+    current_link_width = int(binary_string[-4:], 2)
+    current_link_speed = int(binary_string[-10:-4], 2)
+    return current_link_width, current_link_speed
 
-    #get a list of all bdfs with header type 1
-    header_bdf_list = [bdf for bdf in all_bdf_list if get_header_type(bdf).startswith("01")]
+def read_secondary_bus_number(bus):
+    try:
+        secondary_bus_output = subprocess.check_output(["setpci", "-s", bus, "19.b"])
+        return secondary_bus_output.decode().strip()
+    except subprocess.CalledProcessError:
+        return None
 
-    physical_slot_numbers = []
-    root_ports = []
+def set_bridge_control(bus, value, password):
+    try:
+        subprocess.run(["sudo", "-S", "setpci", "-s", bus, "3e.w=" + value], input=password.encode(), check=True)
+        print(f"Set Bridge Control for {bus} to {value}")
+    except subprocess.CalledProcessError:
+        print(f"Error setting Bridge Control for {bus}.")
 
-    for i, gpu_bdf in enumerate(gpu_bdf_list):
-        # get the bus for the gpu to compare to secondary bus number
-        current_bus = gpu_bdf.split(":")[0]
-        current_bdf = gpu_bdf
-        port_found = False
-        root_port_found = False
-        print(f"starting {i} GPU")
+def get_train_time(bdf):
+    header_type = read_header(bdf)[-2:]
 
-        # keep traversing up the tree until a valid physical port number is found
-        while(not port_found and not root_port_found):
-            print(f"current bus: {current_bus}")
-            upstream_connection = None
+    # make sure that the bdf we are given has a downstream port to train with
+    if header_type != "01":
+        print("Invalid BDF")
+        return -1
+    
+    # I want to read what the speed and width capabilities of the bdf given and the one that is connected to it underneath
+    primary_link_speed_capabilities = read_link_capabilities(bdf)[-1]
+    secondary_bus = read_secondary_bus_number(bdf) + ":00.0"
+    secondary_link_speed_capabilities =  read_link_capabilities(secondary_bus)[-1]
+    train_speed = min(primary_link_speed_capabilities, secondary_link_speed_capabilities)
 
-            # find the bdf with a secondary bus of our current bus
-            for bdf in header_bdf_list:
-                if get_secondary_bus_number(bdf) == current_bus:
-                    upstream_connection = bdf 
+    primary_link_width_capabilities = hex_to_binary(read_link_capabilities(bdf))[-9:-4]
+    secondary_link_width_capabilities =  hex_to_binary(read_link_capabilities(secondary_bus))[-9:-4]
+    train_width = min(primary_link_width_capabilities, secondary_link_width_capabilities)
 
-            # if no upstream connection is found, we are at the root port, report and add to list
-            if upstream_connection is None:
-                print(f"did not find a port with secondary bus as {current_bus}")
-                root_port_found = True
-                root_ports.append(current_bdf)
-                break
-            else:
-                print("Upstream Connection: " + f"{upstream_connection}")
-                slot_capabilities = read_slot_capabilities(upstream_connection)
-                # Extract the physical slot number from slot capabilities bits [31:19]
-                # Convert from hex to binary to decimal
-                slot_number = int(hex_to_binary(slot_capabilities)[:13], 2)
+    # SBR and measure how much time has ellapsed
+    set_bridge_control(bdf, "0043", "Dell1234")
+    set_bridge_control(bdf, "0003", "Dell1234")
+    start = time.time()
+    while(read_link_status(bdf)[-1] != train_speed and hex_to_binary(read_link_status(bdf))[-9:-4] != train_width):
+        pass
+    end = time.time()
+    train_time = end - start
+    return train_time
 
-                print(f"slot_number: {slot_number}")
+    
 
-            # We only want relevant physical ports to our system, in this case 21 to 29
-            if(slot_number in range(21,29) and port_found is False):
-                physical_slot_numbers.append(slot_number)
-                port_found = True
-            current_bdf = upstream_connection
-            current_bus = upstream_connection.split(":")[0]
-        
-        # if a valid physical port was not found, report
-        if(not port_found):
-            physical_slot_numbers.append(-1)
-    gpu_streams = {gpuBDF : [physical_slot_numbers[i], root_ports[i]] for i, gpuBDF in enumerate(gpu_bdf_list)}
-    return gpu_streams
+
 
 def main():
-    # check_replay(burn_time=10, gpu_index=[0,2])
-    print(len(gpu_traverse_up()))
+    bdf = "86:00.0"
+    print(get_train_time(bdf))
 
 if __name__ == "__main__":
     main()
